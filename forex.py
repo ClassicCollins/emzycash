50d3db133c93c67557b46bacede1a89dc9a52be7
Ugwuozor Collins <ugwuozorcollinsemezie@gmail.com>
2022-10-17 15:01:29 +0100

    Add files via upload

---
+++ forex.py
@@ -0,0 +1,14 @@
+import streamlit as st
+#from PIL import Image
+
+
+
+#image = Image.open("emzyCash_background.jpg")
+#st.image(image, caption='Think EmzyCash, Think Easy Cash')
+st.image("forex_background.jpg", caption='Think Easy Cash, Think EmzyCash', width=600, use_column_width= "always")
+# Caption
+st.write("""
+# EmzyCash
+## [Home](https://emzycash-financial-assets-investment-financial-assets-iu5i0y.streamlitapp.com "Click to return home")
+Kindly reach out to emezieugwuozor@gmail.com to optimize your investment in Forex.
+""")
