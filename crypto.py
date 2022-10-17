78651fd864f79097e19eccba62f3e79e931ae276
Ugwuozor Collins <ugwuozorcollinsemezie@gmail.com>
2022-10-17 15:00:05 +0100

    Add files via upload

---
+++ crypto.py
@@ -0,0 +1,19 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+from datetime import date, datetime
+from dateutil.relativedelta import relativedelta
+from matplotlib import pyplot as plt
+#from PIL import Image
+
+
+
+#image = Image.open("emzyCash_background.jpg")
+#st.image(image, caption='Think EmzyCash, Think Easy Cash')
+st.image("crypto-background.jpeg", caption='Think Easy Cash, Think EmzyCash', width=600, use_column_width= "always")
+# Caption
+st.write("""
+# EmzyCash
+## [Home](https://emzycash-financial-assets-investment-financial-assets-iu5i0y.streamlitapp.com "Click to return home")
+Kindly reach out to emezieugwuozor@gmail.com to optimize your investment in cryptocurrency.
+""")
