abded4ff4c1497e28749f711ba3a4969e90f4201
Ugwuozor Collins <ugwuozorcollinsemezie@gmail.com>
2022-10-17 21:59:43 +0100

    Update forex.py

--- forex.py
+++ forex.py
@@ -1,8 +1,6 @@
 import streamlit as st
 #from PIL import Image
 
-
-
 #image = Image.open("emzyCash_background.jpg")
 #st.image(image, caption='Think EmzyCash, Think Easy Cash')
 st.image("forex_background.jpg", caption='Think Easy Cash, Think EmzyCash', width=600, use_column_width= "always")
