22c494ed96f29ed8d678919f88b28271ec33129c
Ugwuozor Collins <ugwuozorcollinsemezie@gmail.com>
2022-12-10 03:06:37 +0100

    Update lagos_markets.py

--- lagos_markets.py
+++ lagos_markets.py
@@ -7,11 +7,11 @@ st.image("lagos_markets.jpg", caption='Think Easy Cash, Think EmzyCash', width=6
 # Caption
 st.write("""
 # QGIS Analysis
+#### [Home](https://emzycash.streamlitapp.com "Click to return home")
 This is analysis carried out on market activities in Local Government Areas of Lagos State using QGIS.
 Entrepreneurs can use this to site their businesses, precision marketing, targeted sales, and geo-coordinated business interventions.
 Further studies can be carried out in each Local Government Area to decide the best location for businesses.
 If this interests you, kindly reach me by email or DM. I'm very open to knowledge sharing and transfer.
-#### [Home](https://emzycash.streamlitapp.com "Click to return home")
 Kindly reach out to emezieugwuozor@gmail.com to discuss further on Market activity analysis using QGIS.
 If you want to view an interactive (web) map built by me using QGIS and  qgis2web plug-in on US sunrise Sunset time and population. Click on the link below
 #### [WebMap](https://classiccollins.github.io/sunlight_in_US/)
