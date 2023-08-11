ce0b34cca0d3832cf44a838ebb9b8c6ab9f1416c
Ugwuozor Collins <ugwuozorcollinsemezie@gmail.com>
2023-08-11 03:04:29 +0100

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -116,7 +116,7 @@ timeDf = pd.DataFrame(timeChoices)
 
 # This takes input from the user on which stock ticker that they would like to look at and gives a sliding scale to
 # choose how much data to look back at in months
-tickerSymbol = st.text_input("Enter a ticker symbol for selected company: ", "AAPL")
+tickerSymbol = st.text_input("Enter a ticker symbol for selected company: ", "MSFT")
 timeChoiceSlider = st.select_slider("Choose the type of data you would like to pull: ",
                                     options=["Daily", "Weekly", "Monthly", "Quarterly"])
 timeSlider = st.slider("Number of Previous %s" % timeDf[timeChoiceSlider][0], min_value=1,
