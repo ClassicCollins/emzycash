aee52569ca0e9c747b61e2092b3e45c93c54596b
Ugwuozor Collins <ugwuozorcollinsemezie@gmail.com>
2023-09-23 22:00:40 +0100

    Update emzyOptimizer.py

--- emzyOptimizer.py
+++ emzyOptimizer.py
@@ -14,11 +14,14 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 from matplotlib.colors import ListedColormap
-import matplotlib.pyplot as plt
 from datetime import datetime
 from io import BytesIO
 import seaborn as sns
 sns.set_style("darkgrid")
+import matplotlib.pyplot as plt
+# Set the "seaborn" style
+plt.style.use("seaborn")
+
 def plot_cum_returns(data, title):
 	daily_cum_returns = 1 + data.dropna().pct_change()
 	daily_cum_returns = daily_cum_returns.cumprod()*100
